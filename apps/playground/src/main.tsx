4987cc1c6eabb58a104ca82c2743f606908ee36f
Melvyn Claes <claes.melvyn@gmail.com>
2026-02-21 21:21:48 +0100

    feat: wrap App component with UICoreProvider for context support

--- apps/playground/src/main.tsx
+++ apps/playground/src/main.tsx
@@ -1,3 +1,4 @@
+import { UICoreProvider } from '@melv1c/ui-core';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App.tsx';
@@ -5,6 +6,8 @@ import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <UICoreProvider>
+      <App />
+    </UICoreProvider>
   </StrictMode>,
 );
